50ba03268328f6f8af1e66d9c53afc022d732ae4
ValeriaZampetti <vzampetti@correo.unimet.edu.ve>
2022-05-11 15:33:40 +0000

    Ejercicio listo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let end = [".com", ".net", ".us", ".io", ".etc"];
+
+  document.querySelector("#newdomain").innerHTML =
+    pronoun[Math.floor(Math.random() * pronoun.length)] +
+    adj[Math.floor(Math.random() * adj.length)] +
+    noun[Math.floor(Math.random() * noun.length)] +
+    end[Math.floor(Math.random() * end.length)];
 };
